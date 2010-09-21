2642e8620f8c736a04e864aab9602d1cdc161bbc
Sergey Martynov <martynovs@gmail.com>
2010-09-21 16:13:53 +0400

    add remote image

---
+++ Classes/RemoteImage.h
@@ -0,0 +1,33 @@
+#import <UIKit/UIKit.h>
+
+@class RemoteImage;
+
+@protocol RemoteImageDelegate <NSObject>
+- (void)remoteImageDidFinishLoading:(RemoteImage*)remoteImage;
+- (void)remoteImage:(RemoteImage*)remoteImage loadingFailedWithError:(NSError*)error;
+@end
+
+
+@interface RemoteImage : NSObject {
+	id<RemoteImageDelegate> delegate;
+	NSURL *imageUrl;
+	NSURLConnection *activeConnection;
+	NSMutableData *connectionData;
+	NSError *lastError;
+	NSData *imageData;
+	UIImage *image;
+}
+@property (nonatomic, assign) id<RemoteImageDelegate> delegate;
+@property (nonatomic, readonly) NSURL *imageUrl;
+@property (nonatomic, readonly) NSData *imageData;
+@property (nonatomic, readonly) UIImage *image;
+@property (nonatomic, readonly) NSError *lastError;
+@property (nonatomic, readonly) BOOL loading;
+
+- (id)initWithURL:(NSURL*)url;
+
+- (void)startLoading;
+- (void)stopLoading;
+- (void)clear:(BOOL)full;
+
+@end
---
+++ Classes/RemoteImage.m
@@ -0,0 +1,97 @@
+#import "RemoteImage.h"
+
+@implementation RemoteImage
+
+@synthesize delegate;
+@synthesize imageUrl;
+@synthesize imageData;
+@synthesize lastError;
+
+- (id)initWithURL:(NSURL*)url {
+	if (![super init]) return nil;
+	imageUrl = [url retain];
+	return self;
+}
+
+
+- (void)dealloc {
+	[imageUrl release];
+	[activeConnection release];
+	[connectionData release];
+	[lastError release];
+	[imageData release];
+	[image release];
+	[super dealloc];
+}
+
+
+- (UIImage*)image {
+	if (!image && imageData) {
+		image = [[UIImage alloc] initWithData:imageData];
+	}
+	return image;
+}
+
+
+- (BOOL)loading {
+	return activeConnection != nil;
+}
+
+
+- (void)startLoading {
+	if (imageData || activeConnection) return;
+	NSURLRequest *request = [NSURLRequest requestWithURL:imageUrl];
+	activeConnection = [[NSURLConnection connectionWithRequest:request delegate:self] retain];
+	[activeConnection scheduleInRunLoop:[NSRunLoop mainRunLoop] forMode:NSDefaultRunLoopMode];
+	[activeConnection start];
+}
+
+
+- (void)stopLoading {
+	[activeConnection cancel];
+	[activeConnection release];
+	activeConnection = nil;
+	[connectionData release];
+	connectionData = nil;
+}
+
+
+- (void)clear:(BOOL)full {
+	[image release];
+	image = nil;
+	if (full) {
+		[imageData release];
+		imageData = nil;
+	}
+}
+
+#pragma mark URL Connection Delegate
+
+- (void)connection:(NSURLConnection*)connection didReceiveResponse:(NSURLResponse*)response {
+}
+
+
+- (void)connection:(NSURLConnection*)connection didReceiveData:(NSData*)data {
+	if (!connectionData) {
+		connectionData = [[NSMutableData alloc] initWithData:data];
+	} else {
+		[connectionData appendData:data];
+	}
+}
+
+
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection {
+	[imageData release];
+	imageData = [connectionData retain];
+	[self stopLoading];
+	[delegate remoteImageDidFinishLoading:self];
+}
+
+
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error {
+	[self stopLoading];
+	lastError = [error retain];
+	[delegate remoteImage:self loadingFailedWithError:error];
+}
+
+@end
